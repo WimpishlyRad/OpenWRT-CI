cda9150c161612319453f0026b9f7c4258980343
WimpishlyRad <ghost1543wdf@gmail.com>
2026-02-25 11:04:57 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -75,6 +75,9 @@ UPDATE_PACKAGE "quickfile" "sbwml/luci-app-quickfile" "main"
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus"
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
 
+UPDATE_PACKAGE "turboacc" "chenmozhijin/turboacc" "luci"
+
+
 #更新软件包版本
 UPDATE_VERSION() {
 	local PKG_NAME=$1
